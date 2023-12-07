f757d8ed132073c7cae0cd8b0c56d123fb33b887
Brendan <brendan.r.le@gmail.com>
2023-12-06 21:50:05 -0600

    edited webpackage

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,20 +31,11 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
+        name: 'Just Another Text Editor',
+        short_name: 'Jate',
+        description: 'A text editor',
         start_url: './',
         publicPath: './',
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
       }),
     ],
 
